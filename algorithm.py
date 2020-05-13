3bfcdb9e0cd372131180614bce881c83420b0840
rafallewanczyk <rafallewanczyk@gmail.com>
2020-05-14 01:07:22 +0200

    fully working main window logic

--- algorithm.py
+++ algorithm.py
@@ -10,8 +10,10 @@ class Data:
 
     def __init__(self, train_images_path, train_labels_path, validation_images_path, validation_labels_path):
 
+        self.is_prime = np.vectorize(self.is_prime)
+
         if train_images_path[-3:] == '.gz':
-            self.train_images= gzip.open(train_images_path, "r")
+            self.train_images = gzip.open(train_images_path, "r")
             if int.from_bytes(self.train_images.read(4), 'big') == 2051:
                 self.train_images.read(12);
                 self.X = self.generate_matrices(self.train_images.read(), self.IMAGE_SIZE)
@@ -21,10 +23,10 @@ class Data:
             self.error += "Zbiór treningowy obrazów - niepoprawne rozszerzenie pliku\n"
 
         if train_labels_path[-3:] == '.gz':
-            self.train_labels= gzip.open(train_labels_path, "r")
+            self.train_labels = gzip.open(train_labels_path, "r")
             if int.from_bytes(self.train_labels.read(4), 'big') == 2049:
                 self.train_labels.read(4);
-                self.y = self.generate_matrices(self.train_labels.read(), 1)
+                self.y = self.is_prime(self.generate_matrices(self.train_labels.read(), 1))
             else:
                 self.error += "Zbiór treningowy etykiet- niepoprwany header plikui\n"
         else:
@@ -44,22 +46,27 @@ class Data:
             self.validation_labels = gzip.open(validation_labels_path, "r")
             if int.from_bytes(self.validation_labels.read(4), 'big') == 2049:
                 self.validation_labels.read(4);
-                self.v_y = self.generate_matrices(self.validation_labels.read(), 1)
+                self.v_y = self.is_prime(self.generate_matrices(self.validation_labels.read(), 1))
             else:
                 self.error += "Zbiór walidacyjny etykiet- niepoprwany header plikui\n"
         else:
             self.error += "Zbiór walidacyjny etykiet- niepoprawne rozszerzenie pliku\n"
 
-
-        if(self.error == ""):
+        if (self.error == ""):
             if self.X.shape[0] != self.y.shape[0]:
-               self.error += "Dane treningowe - niezgodne wymiary tablic\n"
+                self.error += "Dane treningowe - niezgodne wymiary tablic\n"
             if self.v_X.shape[0] != self.v_y.shape[0]:
                 self.error += "Dane walidacyjne- niezgodne wymiary tablic\n"
 
         if self.error == "":
-            print("poprawnie wczytano dane")
-            #todo clear data
+            y_map = self.generate_map(self.y)
+            vy_map = self.generate_map(self.v_y)
+            self.X = np.delete(self.X, y_map, 0)
+            self.y = np.delete(self.y, y_map, 0)
+            self.v_X = np.delete(self.v_X, vy_map, 0)
+            self.v_y = np.delete(self.v_y, vy_map, 0)
+
+            # todo clear data
 
     def getSizes(self):
         return self.X.shape[0], self.v_X.shape[0]
@@ -71,37 +78,96 @@ class Data:
             return 0
         return -1
 
-    # is_prime = np.vectorize(is_prime)
-
     def generate_matrices(self, buffer, length):
         M = np.frombuffer(buffer, dtype=np.uint8)
         M = M.reshape(int(M.shape[0] / (length ** 2)), -1)
 
         return M
 
-    def generate(self):
-        print(self.y)
-#
-# def generate_map(labels):
-#     map = np.argwhere(labels == -1)
-#     return map.T[0]
-#
-#
-# X = generate_matrices(train_image.read(), IMAGE_SIZE)
-# y = is_prime(generate_matrices(train_labels.read(), 1))
-# map = generate_map(y)
-#
-# X = np.delete(X, map, 0)
-# y = np.delete(y, map, 0)
-#
-#
-#
-# v_X = generate_matrices(validation_image.read(), IMAGE_SIZE)
-# v_y = is_prime(generate_matrices(validation_labels.read(), 1))
-# map = generate_map(v_y)
-#
-# v_X = np.delete(v_X, map, 0)
-# v_y = np.delete(v_y, map, 0)
-#
-# print(X.shape)
-# print(y.shape)
+    def generate_map(self, labels):
+        map = np.argwhere(labels == -1)
+        return map.T[0]
+
+
+class Model():
+    IMAGE_SIZE = 28
+
+    def __init__(self, step_size=0.1, epochs=8, momentum=0.9, reg=0.00001, verbose=True, write_line = None):
+        self.theta = np.random.rand(self.IMAGE_SIZE ** 2, 1)
+        self.V = np.random.rand(self.IMAGE_SIZE ** 2, 1)
+        self.step_size = step_size
+        self.epochs = epochs
+        self.momentum = momentum
+        self.reg = reg
+        self.verbose = verbose
+        self.write_line = write_line
+
+    def __sigma(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def __cost(self, theta, X, y):
+        cost = y * np.log(self.__sigma(np.matmul(X, self.theta)) + 1e-7) + (1 - y) * np.log(
+            1 - self.__sigma(np.matmul(X, self.theta)) + 1e-7)
+        reg_cost = self.reg / 2 * self.theta ** 2
+        return cost.mean() - reg_cost.sum() / X.shape[0]
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        self.write_line(f"Zaczynam trening, szacowany czas {self.epochs * 2}s")
+        y_axis = [self.__cost(self.theta, X, y)]
+
+        start = time.time()
+        iteration_start = start
+        permutation = np.arange(X.shape[0])
+
+        for epoch in range(1, self.epochs + 1):
+            np.random.shuffle(permutation)
+            for i in permutation:
+                gradient = (y[i] - self.__sigma(np.matmul(self.theta.T, X[i]))) * X[i].reshape(self.IMAGE_SIZE ** 2,
+                                                                                               1) - 2 * self.reg * self.theta
+                self.V = self.momentum * self.V + (1 - self.momentum) * gradient
+                self.theta = self.theta + self.step_size * self.V
+
+            self.write_line(f"{epoch}/{self.epochs} iteracja zakonczona po {time.time() - iteration_start}")
+            iteration_start = time.time()
+
+            if (self.verbose == True):
+                y_axis.append(self.__cost(self.theta, X, y))
+
+        if (self.verbose == True):
+            x_axis = np.arange(0, len(y_axis))
+            self.figure = plt.Figure()
+            self.figure.clear()
+            ax = self.figure.add_subplot(111)
+            ax.plot(x_axis, y_axis)
+            self.figure.suptitle(f'epochs:{self.epochs}, step_size:{self.step_size}, momentum:{self.momentum},l2:{self.reg}')
+
+            # plt.show()
+
+
+        self.write_line(f"trening zakonczony po {time.time() - start}")
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return self.__sigma(X @ self.theta)
+
+    @staticmethod
+    def evaluate(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        correct = 0
+        total = 0
+        for i in range(y_true.shape[0]):
+            if y_true[i] == np.rint(y_pred[i]):
+                correct += 1
+            total += 1
+        return correct / total
+
+    def confiuson_matrix(self, y_true, y_pred):
+        true_positive = true_negative = false_positive = false_negative = 0
+        for i in range(y_true.shape[0]):
+            if y_true[i] == np.rint(y_pred[i]) and y_true[i] == 1:
+                true_positive += 1
+            elif y_true[i] == np.rint(y_pred[i]) and y_true[i] == 0:
+                true_negative += 1
+            elif y_true[i] != np.rint(y_pred[i]) and y_true[i] == 1:
+                false_negative += 1
+            elif y_true[i] != np.rint(y_pred[i]) and y_true[i] == 0:
+                false_positive += 1
+        return true_positive, true_negative, false_positive, false_negative
--- logic.py
+++ logic.py
@@ -1,40 +1,32 @@
+from matplotlib.figure import Figure
+
 from form import *
 from PyQt5.QtWidgets import QFileDialog
-from algorithm import Data
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from algorithm import Data, Model
 from configuration import *
 import json
 
 
 class logic(Ui_MainWindow):
-    # self.startButton.clicked.connect(self.click)
-    # self.closeButton.clicked.connect(self.click_exit);
 
     def __init__(self, MainWindow):
         self.setupUi(MainWindow)
-        self.startButton.clicked.connect(self.click)
-        self.startButton.setEnabled(False)
-        self.closeButton.clicked.connect(self.click_exit)
+        self.startButton.clicked.connect(self.start)
+        self.startButton.setEnabled(True)
+        self.closeButton.clicked.connect(self.clear)
 
-        self.maxTrainingValidator = QtGui.QIntValidator()
-        self.maxValidationValidator = QtGui.QIntValidator()
-        self.epochValidator = QtGui.QIntValidator()
+        self.stepSize.valueChanged.connect(lambda: self.stepSizeSlider.setValue(int(self.stepSize.value() * 100)))
+        self.l2.valueChanged.connect(lambda: self.l2Slider.setValue(int(self.l2.value() * 100)))
+        self.momentum.valueChanged.connect(lambda: self.momentumSlider.setValue(int(self.momentum.value() * 100)))
+        self.epoch.valueChanged.connect(lambda: self.epochSlider.setValue(self.epoch.value()))
 
-        self.trainingExampleNum.setValidator(self.maxTrainingValidator)
-        self.validationExampleNum.setValidator(self.maxValidationValidator)
-        self.epochNumber.setValidator(self.epochValidator)
-
-        self.stepSize.valueChanged.connect(lambda : self.stepSizeSlider.setValue(int(self.stepSize.value() * 100)))
-        self.l2.valueChanged.connect(lambda : self.l2Slider.setValue(int(self.l2.value() * 100)))
-        self.momentum.valueChanged.connect(lambda : self.momentumSlider.setValue(int(self.momentum.value() * 100)))
-
-        self.stepSizeSlider.valueChanged.connect(lambda: self.stepSize.setValue(float(self.stepSizeSlider.value()/100)))
-        self.l2Slider.valueChanged.connect(lambda: self.l2.setValue(float(self.l2Slider.value()/100)))
-        self.momentumSlider.valueChanged.connect(lambda: self.momentum.setValue(float(self.momentumSlider.value()/100)))
-
-
-        # self.trainingExampleNum.textChanged.connect(self.check_state)
-        # self.trainingExampleNum.textChanged.emit(self.trainingExampleNum.text())
-        # self.validationExampleNum.textChanged.connect(self.check_state)
+        self.stepSizeSlider.valueChanged.connect(
+            lambda: self.stepSize.setValue(float(self.stepSizeSlider.value() / 100)))
+        self.l2Slider.valueChanged.connect(lambda: self.l2.setValue(float(self.l2Slider.value() / 100)))
+        self.momentumSlider.valueChanged.connect(
+            lambda: self.momentum.setValue(float(self.momentumSlider.value() / 100)))
+        self.epochSlider.valueChanged.connect(lambda: self.epoch.setValue(self.epochSlider.value()))
 
         self.trainingImageButton.clicked.connect(self.selectTrainingImagePath)
         self.trainingLabelsButton.clicked.connect(self.selectTrainingLabelPath)
@@ -46,14 +38,36 @@ class logic(Ui_MainWindow):
 
         self.loadFilesButton.clicked.connect(self.loadData)
 
-        self.is_data_loaded = False;
+        self.is_data_loaded = False
 
-        # self.stepSize.textChanged.emit(self.stepSize.text())
 
     def print_pressed(self):
         print("pressed")
 
+    def write_line(self, text):
+        item = self.verticalLayout_3.takeAt(self.verticalLayout_3.count() -1)
+        _translate = QtCore.QCoreApplication.translate
+        new = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        new.setObjectName("new")
+        self.verticalLayout_3.addWidget(new)
+        new.setText(_translate("MainWindow", text))
+        self.verticalLayout_3.addItem(item)
+        self.scrollAreaWidgetContents.adjustSize()
+        app.processEvents()
 
+    def draw_plot(self, plot):
+        item = self.verticalLayout_3.takeAt(self.verticalLayout_3.count() -1)
+        self.verticalLayout_3.addWidget(plot)
+        self.verticalLayout_3.addItem(item)
+        self.scrollAreaWidgetContents.adjustSize()
+        app.processEvents()
+
+    def clear(self):
+        while self.verticalLayout_3.count() > 1 :
+            item = self.verticalLayout_3.takeAt(0)
+            item.widget().setParent(None)
+            item = None
+        app.processEvents()
 
 
     def save_configuration(self):
@@ -77,28 +91,30 @@ class logic(Ui_MainWindow):
             new_config = (configuration(**json.loads(json_data)))
             self.load_configuration(new_config)
         else:
-            print("nieprawidlowy plik")
-
+            self.write_line("Nieprawidłowy plik")
     def load_configuration(self, config):
         _translate = QtCore.QCoreApplication.translate
         self.trainingImagePath.setText(_translate("MainWindow", config.training_images_path))
         self.trainingLabelsPath.setText(_translate("MainWindow", config.training_labels_path))
         self.validationImagePath.setText(_translate("MainWindow", config.validation_images_path))
         self.validationLabelPath.setText(_translate("MainWindow", config.validation_labels_path))
+        self.write_line("Wczytano konfigurację")
+
+    def start(self):
+        self.clear()
+
+        model = Model(self.stepSize.value(), self.epoch.value(), self.momentum.value(), self.l2.value(), True, self.write_line)
+        model.fit(self.data.X, self.data.y)
+        self.canvas = FigureCanvasQTAgg(model.figure)
+        # self.verticalLayout_3.addWidget(self.canvas)
+        self.draw_plot(self.canvas)
+
 
     def click(self):
-        item = self.verticalLayout_3.itemAt(self.counter + 2)
-        print(item)
-        self.verticalLayout_3.removeItem(item)
-        # self.spacerItem.setParent(-1)
-        _translate = QtCore.QCoreApplication.translate
-        new = QtWidgets.QLabel(self.scrollAreaWidgetContents)
-        new.setObjectName("new")
-        self.verticalLayout_3.addWidget(new)
-        new.setText(_translate("MainWindow", f"nowy label{self.counter}"))
-        self.counter += 1
+        counter = self.verticalLayout_3.count()
+        print(counter)
+        self.write_line("adf")
 
-        self.verticalLayout_3.addItem(item)
 
     def selectTrainingImagePath(self):
         self.open_dialog_box(self.trainingImagePath)
@@ -116,7 +132,7 @@ class logic(Ui_MainWindow):
         self.data = Data(self.trainingImagePath.text(), self.trainingLabelsPath.text(), self.validationImagePath.text(),
                          self.validationLabelPath.text())
         if self.data.error != "":
-            print(self.data.error)
+            self.write_line(self.data.error)
             self.trainingExampleNum.setEnabled(False)
             self.validationExampleNum.setEnabled(False)
         else:
@@ -125,33 +141,30 @@ class logic(Ui_MainWindow):
 
             _translate = QtCore.QCoreApplication.translate
             self.maxTraining.setText(_translate("MainWindow", f"Max:{self.data.getSizes()[0]}"))
+            self.trainingExampleNum.setMaximum(self.data.getSizes()[0])
             self.maxValidation.setText(_translate("MainWindow", f"Max:{self.data.getSizes()[1]}"))
-            self.maxTrainingValidator.setRange(0, self.data.getSizes()[0])
-            self.maxValidationValidator.setRange(1, self.data.getSizes()[1])
+            self.validationExampleNum.setMaximum(self.data.getSizes()[1])
+            self.maxValidation.setText(_translate("MainWindow", f"Max:{self.data.getSizes()[1]}"))
+            self.startButton.setEnabled(True)
             self.is_data_loaded = True
+            self.write_line("Poprawnie wczytano dane\n")
 
     def open_dialog_box(self, label):
         filename = QFileDialog.getOpenFileName()
 
-        _translate = QtCore.QCoreApplication.translate
-        label.setText(_translate("MainWindow", filename[0]))
+        if filename[0] != "":
+            _translate = QtCore.QCoreApplication.translate
+            label.setText(_translate("MainWindow", filename[0]))
 
     def click_exit(self):
         exit()
 
-    def check_state(self, *args, **kwargs):
-        print("check state works")
-        sender = self.sender()
-        print(sender)
-        # state = validator.validate(self.stepSize.text(), 0)[0]
-        # if state == QtGui.QValidator.Acceptable:
-        #     color = '#c4df9b'  # green
-        # elif state == QtGui.QValidator.Intermediate:
-        #     color = '#fff79a'  # yellow
-        # else:
-        #     color = '#f6989d'  # red
-        # self.stepSize.setStyleSheet('QLineEdit { background-color: %s }' % color)
+class MplCanvas(FigureCanvasQTAgg):
 
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 if __name__ == "__main__":
     import sys
